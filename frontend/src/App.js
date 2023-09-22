1ff766c6a20975aadb0ad177bbf9ca75bff32ee9
Andy <olshansa@gmail.com>
2023-09-22 15:37:19 -0400

    Added developer's map... needs to be updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MapContainer from './MapContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <MapContainer/>
     </div>
   );
 }
