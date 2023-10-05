410fc92a4ee0aae2c776f57973f259fee944efaf
Andy <olshansa@gmail.com>
2023-10-05 18:08:55 -0400

    added a userPic... fuggedaboutit

--- frontend/src/BeaconInfo.js
+++ frontend/src/BeaconInfo.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const BeaconInfo = ({ username, startTime, endTime, gameTitle, miscInfo, gamePic }) => {
+const BeaconInfo = ({ username, startTime, endTime, gameTitle, miscInfo, gamePic, userPic }) => {
     return (
-        <div className="bg-white rounded-lg w-full max-w-md mx-auto shadow-lg my-5 p-10 flex justify-between">
-            <div className='ml-20 text-left' >
-                <h1 className="text-2xl font-bold mb-2">{username}</h1>
-                <h2 className="text-xl mb-2">{gameTitle}</h2>
-                <p className="mb-2">{startTime} - {endTime}</p>
-                <p className="mb-2">Misc Info: <span className="font-light">{miscInfo}</span></p>
+        <div className="bg-white rounded-lg w-full max-w-md mx-auto shadow-lg my-5 p-2 px-2 flex justify-between">
+            <div className='ml-2 mt-2 text-left'>
+                <div className="flex items-baseline mb-2">
+                    <img src={userPic} alt="Game Pic" className="top-0 right-0 h-16 w-16 mr-4" />
+                    <h1 className="text-2xl font-bold">{username}</h1>
+                </div>
+                <div className='ml-4'>
+                    <h2 className="text-xl mb-2">{gameTitle}</h2>
+                    <p className="mb-2">{startTime} - {endTime}</p>
+                    <p className="mb-2">Misc Info: <span className="font-light">{miscInfo}</span></p>
+                </div>
             </div>
-            <img src={gamePic} alt="Game Pic" className="top-0 right-0 h-16 w-16 rounded-full" />
+            <img src={gamePic} alt="Game Pic" className="top-0 right-0 h-32 w-32 rounded-full" />
         </div>
     )
 };
@@ -20,7 +25,8 @@ BeaconInfo.defaultProps = {
     endTime: "end",
     gameTitle: "game title",
     miscInfo: "misc info",
-    gamePic: "images/catCry.jpg"
+    gamePic: "images/catCry.jpg",
+    userPic: "images/catCry.jpg"
 };
 
 export default BeaconInfo;
