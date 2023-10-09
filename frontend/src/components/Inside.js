226be1fa2a3b4b064bcce227b148ec13678c9a4b
del-cj <del.cj126@gmail.com>
2023-10-09 01:59:49 -0400

    Styled the login page

--- frontend/src/components/Inside.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Inside() {
-    return (
-        <h1>Welcome In!</h1>
-    );
-}
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import "./login.css";
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -8,22 +9,25 @@ const Login = () => {
         e.preventDefault();
         console.log(email);
     }
-    
+
     return (
-        <div className="login-wrapper">
-            <form onSubmit={handleSubmit}>
-                <label for="email">
-                    Email:
-                </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
-                <label>
-                    Password:
-                </label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
-                <div>
-                    <button type="submit">Log In</button>
-                </div>
-            </form>
+        <div className="login-container">
+            <h1><strong>Welcome<br/>Back!</strong></h1>
+            <div className="login-wrapper" >
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <p>Email:</p>
+                    </label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
+                    <label>
+                        <p>Password:</p>
+                    </label>
+                    <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
+                    <div className="submit-button">
+                        <button type="submit">Log In</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
