5764c0a7ab6337ba895a8edd55adfa0710630225
del-cj <del.cj126@gmail.com>
2023-10-09 14:59:00 -0400

    commit issue (?)

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import "./login.css";
 
@@ -35,43 +34,4 @@ const Login = () => {
     );
 }
 
-=======
-import React, { useState } from "react";
-import "./login.css";
-import "./fonts/Inkbleed-Regular.ttf";
-
-
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-        console.log(pass);
-    }
-
-    return (
-        <div className="login-container">
-            <h1><strong>Welcome<br/>Back!</strong></h1>
-            <div className="login-wrapper" >
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <p>Email:</p>
-                    </label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
-                    <label>
-                        <p>Password:</p>
-                    </label>
-                    <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
-                    <div className="submit-button">
-                        <button type="submit">Log In</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-}
-
->>>>>>> fa65e9ebd969ab96a2dd177b9879d6f54b76c04a
 export default Login;
