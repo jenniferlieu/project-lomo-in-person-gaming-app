f8ea2299c9cc6bfc6562242cb6e352cf0de03814
Andy <olshansa@gmail.com>
2023-10-11 15:00:12 -0400

    Merged beaconInfo and circle into single component

--- frontend/src/Beacon.js
+++ frontend/src/Beacon.js
@@ -1,5 +1,50 @@
-/*
-TODO: 
-Link the circle with the beacon info to put int the mapContainer
-Maybe create a custom beaconPin component for the circle so we can control it better?
-*/
+import React, { useState } from 'react';
+import { Circle } from '@react-google-maps/api';
+import BeaconInfo from './BeaconInfo';
+
+const Beacon = ({ mapRef, setMapCenter }) => {
+  const circle = {
+    center: {
+      lat: 40,
+      lng: -75
+    },
+    radius: 2000, // Radius in meters 
+    options: {
+      fillColor: '#FF0000', 
+      strokeColor: '#0000FF', 
+      strokeOpacity: 0.8, 
+      strokeWeight: 2, 
+    },
+  };
+
+  const [showBeaconInfo, setShowBeaconInfo] = useState(false);
+
+  const toggleDisplayBeacon = () => {
+    setShowBeaconInfo(!showBeaconInfo);
+    if (!showBeaconInfo) {
+      setMapCenter(mapRef.current.getCenter());
+      mapRef.current.setOptions({ gestureHandling: 'none' });
+    } else {
+      mapRef.current.setOptions({ gestureHandling: 'cooperative' });
+    }
+  }
+  
+  /*
+  TODO: Add failsafe unfreeze via an 'X' button
+  */
+
+  return (
+    <>
+      <Circle {...circle} onClick={toggleDisplayBeacon} />
+      {showBeaconInfo && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="w-96 h-48">
+            <BeaconInfo />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Beacon;
--- frontend/src/MapContainer.js
+++ frontend/src/MapContainer.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from 'react';
-import { GoogleMap, LoadScript, Circle } from '@react-google-maps/api';
-import BeaconInfo from './BeaconInfo';
+import React, { useRef, useState } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import Beacon from './Beacon';
 
 const MapContainer = () => {
   const mapStyles = {
@@ -13,43 +13,10 @@ const MapContainer = () => {
     lng: -75.1554, // default longitude
   };
 
-  const [showBeaconInfo, setShowBeaconInfo] = useState(false);
   const [mapCenter, setMapCenter] = useState(defaultCenter);
   const mapRef = useRef(null);
 
-  const circle = {
-    center: {
-      lat: 40,
-      lng: -75
-    },
-    radius: 2000, // Radius in meters 
-    options: {
-      fillColor: '#FF0000', 
-      strokeColor: '#0000FF', 
-      strokeOpacity: 0.8, 
-      strokeWeight: 2, 
-    },
-  };
-
   const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-
-  /*
-    TODO:
-    The circle could be in the center of the map and have the beaconInfo displayed overtop of the circle
-    In this case, there is no way to unfreeze everything
-    2 fixes to add:
-      1) make it so that if you click on another beacon, that information will be displayed instead
-      2) add a button to hide the beacon and go back to the map
-    Discuss with team
-*/
-  const toggleDispalyBeacon = () => {
-    setMapCenter(mapRef.current.getCenter());
-    setShowBeaconInfo(!showBeaconInfo);
-  }
-
-  const mapOptions = {
-    gestureHandling: showBeaconInfo ? 'none' : 'cooperative'
-  }
 
   return (
     <div className='flex justify-center items-center h-screen'>
@@ -58,19 +25,11 @@ const MapContainer = () => {
           mapContainerStyle={mapStyles}
           zoom={10}
           center={mapCenter}
-          options={mapOptions}
           onLoad={(map) => {
             mapRef.current = map;
           }}
         >
-          {showBeaconInfo && (
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              <div className="w-96 h-48">
-                <BeaconInfo />
-              </div>
-            </div>
-          )}
-          <Circle {...circle} onClick={toggleDispalyBeacon} />
+          <Beacon mapRef={mapRef} setMapCenter={setMapCenter} />
         </GoogleMap>
       </LoadScript>
     </div>
