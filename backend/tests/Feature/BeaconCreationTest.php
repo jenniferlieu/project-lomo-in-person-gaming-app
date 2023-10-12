244f8ceb3d4d7acc1fdcf5cce9a845260e49e90f
Jennifer Lieu <jennilieu@gmail.com>
2023-10-12 00:38:39 -0400

    Update beacon creation test file name

--- backend/tests/Feature/BeaconCreationTest.php
+++ backend/tests/Feature/BeaconCreationTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class BeaconTest extends TestCase
+class BeaconCreationTest extends TestCase
 {
     /**
      * Tests the post request to the '/api/beacons' route to create a new beacon.
@@ -14,7 +14,9 @@ class BeaconTest extends TestCase
      */
     public function test_post_request_returns_successful_response(): void
     {
-        $response = $this->post('/api/beacons', [
+        $response = $this->withHeaders([
+            'Accept' => 'application/json'
+        ])->post('/api/beacons', [
             'host_id' => '0',
             'title' => 'Smash brothers',
         ]);
@@ -26,11 +28,14 @@ class BeaconTest extends TestCase
      * Tests that validation checks fail on the POST request for beacons.
      * Should return a 422 status code for unprocessable entity.
      */
-    public function test_validation_failed_on_post_request_returns_error_response(): void
+    public function test_validation_should_fail_on_unsufficient_post_request(): void
     {
         $response = $this->withHeaders([
             'Accept' => 'application/json'
-        ])->post('/api/beacons', ['host_id' => '', 'title' => '',]);
+        ])->post('/api/beacons', [
+            'host_id' => '',  // host_id is required and cannot be empty string
+            'title' => '', // title is required and cannot be empty string
+        ]);
 
         $response->assertStatus(422);
     }
