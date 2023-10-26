2ede0478abaf22237cf259b8238d61a246e05a82
del-cj <del.cj126@gmail.com>
2023-10-25 23:41:23 -0400

    '/' auto redirects to log in if not already

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -6,18 +6,27 @@ import Login from './components/Login/Login.jsx';
 import Signup from './components/signup/Signup.jsx';
 import HomePage from './HomePage.js';
 
-
+//Create authentication context
+const AuthContext = createContext({
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
+});
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = useContext(AuthContext);
+
   return (
     <div className='App'>
-      <Router>
-        <Routes>
-          <Route exact path='/home' element={<HomePage />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/signup' element={<Signup />} />
-        </Routes>
-      </Router>
+      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        <Router>
+          <Routes>
+            <Route exact path='/' element={auth.isLoggedIn ? <HomePage /> : <Navigate to='/login' />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/signup' element={<Signup />} />
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
