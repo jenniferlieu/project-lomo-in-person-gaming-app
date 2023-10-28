3906947f8e7631cc85f462f0ad402f545ffedbaa
JonathanAltenburg <jaltenburg219@gmail.com>
2023-10-28 16:47:00 -0400

    API communication/partial testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,41 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import nock from 'nock';
+import BeaconCreation from './BeaconCreation';
+import { Button } from '@mui/material';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/*
+
+test('confirm beacon creation', () =>{
+  const input = nock('http://localhost')
+    .post('/api/beacons')
+    .once()
+    .reply(201, {
+      data: {
+        host_id,
+        title,
+        game_title,
+        game_image,
+        descirption,
+        date_time,
+        address,
+        latitude,
+        longtitude,
+        players_needed,
+        players_attending,
+        comments,
+        updated_at,
+        created_at,
+        _id
+      }
+    })
+    render(<BeaconCreation/>);
+    BeaconCreation.click(Button.BeaconCreation)
+});
+*/
--- frontend/src/BeaconCreation.js
+++ frontend/src/BeaconCreation.js
@@ -3,8 +3,6 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
-
-
 function BeaconCreation() {
   const [name, setState] = useState("");
   const [game, setGame] = useState("");
@@ -15,98 +13,119 @@ function BeaconCreation() {
   const [timeFrom, setFrom] = useState("");
   const [timeTo, setTo] = useState("");
 
-function onClose() {
-  let data = { // exepected json schema
-    host_id: "1", // required
-    title: name, // required
-    game_title: game, // required
-    game_image: "images/catScream.jpg", //required
-    description: misc,
-    date_time: timeFrom, // required
-    address: location, // required
-    latitude: 40.7580, // required
-    longitude: 73.9795, // required
-    num_players: players, // required
-  };
-  console.log(data); 
-
-  // define url and headers
-let url = "http://localhost/api/hello";
-let options = {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  }
-};
-
-// make api call
-fetch(url, options)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log("response", response);
-  })
-  .catch((error) => console.log("error", error));
+function displayText(text) {
+  document.getElementById("displayArea").innerHTML = text;
+  
 }
+
+  function onClose() {
+    let data = {
+      // exepected json schema
+      host_id: "653578eeab2e358c510a1eb2", // required
+      title: name, // required
+      game_title: game, // required
+      game_system: system, //required
+      description: misc, //required
+      start_date_time: timeFrom, // required
+      end_date_time: timeTo, //required
+      address: location, // required
+      latitude: 40.758, // required
+      longitude: 73.9795, // required
+      num_players: players, // required
+    };
+    console.log(data);
+
+    // define url and headers
+    let url = "http://localhost/api/beacons";
+    let logindata = {
+      email: "pikachu@test.com",
+      password: "secret1234",
+    };
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization:
+          "Bearer 653c9caf1df79134d402c692|uKoK3o20u3kcSIZbooOTl3i8McWMMWd5oUtKoSdab467dec1",
+      },
+      body: JSON.stringify(data),
+    };
+
+    // make api call
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("response", response);
+
+        displayText("Beacon Confirmed!");
+        /*
+        if (response == "<Response [201]>") {
+          
+        }
+        */
+      })
+      .catch((error) => console.log("error", error));
+  }
 
   return (
     <div>
       <div class="bg-white rounded-lg w-full leading-relaxed max-w-md mx-auto shadow-lg my-5 p-2 px-2 text-left absolute">
         <tr>
-        <th classname="min-w-screen min-h-screen bg-black bg-opacity-25 flex justify-center items-center">
-          <label htmlFor={"input"}>Beacon Name</label>
-          <input
-            id={"BeaconName"}
-            type={"text"}
-            value={name}
-            placeholder={"Name"}
-            required
-            onChange={(event) => {
-              setState(event.target.value);
-            }}
-          />
-        </th>
-        <th classname="min-w-screen min-h-screen bg-black bg-opacity-25 flex p-2 justify-center items-center">
-          <label htmlFor={"input2"}>Game Title</label>
-          <input
-            id={"BeaconGame"}
-            type={"text"}
-            value={game}
-            placeholder={"Type Game Here"}
-            required
-            onChange={(event) => {
-              setGame(event.target.value);
-            }}
-          />
-        </th>
+          <th classname="min-w-screen min-h-screen bg-black bg-opacity-25 flex justify-center items-center">
+            <label htmlFor={"input"}>Beacon Name</label>
+            <input
+              id={"BeaconName"}
+              type={"text"}
+              value={name}
+              placeholder={"Name"}
+              required
+              onChange={(event) => {
+                setState(event.target.value);
+              }}
+            />
+          </th>
+          <th classname="min-w-screen min-h-screen bg-black bg-opacity-25 flex p-2 justify-center items-center">
+            <label htmlFor={"input2"}>Game Title</label>
+            <input
+              id={"BeaconGame"}
+              type={"text"}
+              value={game}
+              placeholder={"Type Game Here"}
+              required
+              onChange={(event) => {
+                setGame(event.target.value);
+              }}
+            />
+          </th>
         </tr>
         <tr>
-        <th classname="min-w-screen min-h-screen bg-black  bg-opacity-25 flex p-2 justify-center items-center">
-          <label htmlFor={"input3"}>No. of players</label>
-          <input
-            id={"Players"}
-            type={"text"}
-            value={players}
-            placeholder={"How many Players?"}
-            required
-            onChange={(event) => {
-              setPlayers(event.target.value);
-            }}
-          />
-        </th>
-        <th classname="min-w-screen min-h-screen bg-black bg-opacity-25 flex p-2 justify-center items-center">
-          <label htmlFor={"input4"}>Game System</label>
-          <input
-            id={"BeaconSystem"}
-            type={"text"}
-            value={system}
-            placeholder={"Which System?"}
-            required
-            onChange={(event) => {
-              setSystem(event.target.value);
-            }}
-          />
-        </th>
+          <th classname="min-w-screen min-h-screen bg-black  bg-opacity-25 flex p-2 justify-center items-center">
+            <label htmlFor={"input3"}>No. of players</label>
+            <input
+              id={"Players"}
+              type={"text"}
+              value={players}
+              placeholder={"How many Players?"}
+              required
+              onChange={(event) => {
+                setPlayers(event.target.value);
+              }}
+            />
+          </th>
+          <th classname="min-w-screen min-h-screen bg-black bg-opacity-25 flex p-2 justify-center items-center">
+            <label htmlFor={"input4"}>Game System</label>
+            <input
+              id={"BeaconSystem"}
+              type={"text"}
+              value={system}
+              placeholder={"Which System?"}
+              required
+              onChange={(event) => {
+                setSystem(event.target.value);
+              }}
+            />
+          </th>
         </tr>
         <th classname="min-w-screen min-h-screen bg-black  bg-opacity-25 flex p-2 justify-center items-center">
           <label htmlFor={"input5"}>Location</label>
@@ -122,38 +141,36 @@ fetch(url, options)
           />
         </th>
         <tr>
-        <th classname="min-w-screen min-h-screen border-10 bg-black bg-opacity-25 flex p-2 span-5 justify-center items-center">
-          <label htmlFor={"input6"}>Misc. Info</label>
-          <input
-            id={"MiscInfo"}
-            type={"text"}
-            value={misc}
-            placeholder={"Additional Details"}
-            maxLength={100}
-            onChange={(event) => {
-              setMisc(event.target.value);
-            }}
-          />
-        </th>
+          <th classname="min-w-screen min-h-screen border-10 bg-black bg-opacity-25 flex p-2 span-5 justify-center items-center">
+            <label htmlFor={"input6"}>Misc. Info</label>
+            <input
+              id={"MiscInfo"}
+              type={"text"}
+              value={misc}
+              placeholder={"Additional Details"}
+              maxLength={100}
+              onChange={(event) => {
+                setMisc(event.target.value);
+              }}
+            />
+          </th>
         </tr>
         <div>
           <LocalizationProvider dateAdapter={AdapterDayjs} classname="p-20">
-            <DateTimePicker 
-            label="From: " 
-            value={timeFrom}
-            onChange={(newValue) => setFrom(newValue)}
+            <DateTimePicker
+              label="From: "
+              value={timeFrom}
+              onChange={(newValue) => setFrom(newValue)}
             />
-
           </LocalizationProvider>
         </div>
         <div>
           <LocalizationProvider dateAdapter={AdapterDayjs} classname="p-20">
-            <DateTimePicker 
-            label="To: " 
-            value={timeTo}
-            onChange={(newValue) => setTo(newValue)}
+            <DateTimePicker
+              label="To: "
+              value={timeTo}
+              onChange={(newValue) => setTo(newValue)}
             />
-
           </LocalizationProvider>
         </div>
         <div>
@@ -169,6 +186,7 @@ fetch(url, options)
           >
             Confirm
           </button>
+          <div className="font-bold relative bg-green-400 py-1 px-1 rounded float-right" id="displayArea">Confirmed?</div>
         </div>
       </div>
     </div>
