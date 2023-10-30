1e923cf41fbe72265f527a5e115f227be048caea
Alan <alan.uthuppan@temple.edu>
2023-10-30 13:58:25 -0400

    Test connection web route added.

--- backend/config/database.php
+++ backend/config/database.php
@@ -72,7 +72,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db.lzdmytcapmsswxlabxax.supabase.co'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -80,7 +80,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'schema' => 'public',
             'sslmode' => 'prefer',
         ],
 
