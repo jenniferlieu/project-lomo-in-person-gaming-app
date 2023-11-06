a1f09ac5008e63d828b6b5b6267340a1ec39fc3e
del-cj <del.cj126@gmail.com>
2023-11-06 15:19:15 -0500

    got the styling to work and adjusting the look

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
 }
-
