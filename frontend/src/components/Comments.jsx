1bf5108488b21544ca3bd06273626648a9048be9
del-cj <del.cj126@gmail.com>
2023-11-08 14:28:25 -0500

    tweaks to appearance

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Comment = ({ author, text, onDelete }) => {
     return (
         <div>
             <h3>{author}</h3>
             <p>{text}</p>
-            <button onClick={onDelete}>
-                X
+            <button onClick={onDelete} className='rounded-full bg-sky-700 text-teal-50 text-xs px-3 py-1 text-md self-auto m-2 flex-none'>
+                Delete
             </button>
         </div>
     );
@@ -30,17 +29,19 @@ const CommentSection = () => {
     };
 
     return (
-        <div className='bg-white relative w-full p-2'>
+        <div className='bg-white relative w-full p-4'>
             <h1 className='text-2xl text-sky-950 text-center'>Comment Section</h1>
             <div classname='flex justify-center w-full'>
-                <textarea value={newComment} onChange={(e) => setNewComment(e.target.value)} className='border-2 border-sky-950 w-11/12 left-0 self-center m-auto'/>
-                <button onClick={addComment} className='rounded-full bg-sky-900 text-teal-50 px-3 py-1 text-md self-auto m-2'>
-                    +
+                <textarea value={newComment} onChange={(e) => setNewComment(e.target.value)} className='border-2 border-sky-950 p-1 flex-1 w-full left-0 self-center m-auto'/>
+                <button onClick={addComment} className='rounded-full bg-sky-900 text-teal-50 text-xs px-3 py-1 text-md self-auto m-2 flex-none'>
+                    Add Comment
                 </button>
             </div>
 
             {comments.map((comment) => (
-                <Comment key={comment.id} text={comment.text} onDelete={() => deleteComment(comment.id)} />
+                <div className='border-2 border-teal-100 my-3 p-1 w-full mx-auto shadow-md'>
+                    <Comment key={comment.id} text={comment.text} onDelete={() => deleteComment(comment.id)} />
+                </div>
             ))}
                       
         </div>
