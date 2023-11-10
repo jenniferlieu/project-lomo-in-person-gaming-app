044906cc3e4567227de6f2d20f411cf2e225efd5
Andy <olshansa@gmail.com>
2023-11-10 15:16:07 -0500

    added a join beacon button

--- frontend/src/BeaconInfoWindow.js
+++ frontend/src/BeaconInfoWindow.js
@@ -73,6 +73,9 @@ const BeaconInfoWindow = ({ username, startTime, endTime, gameTitle, miscInfo, g
                   className="h-3 w-3 ml-3"
                 />
               </button>
+              <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Join Beacon
+              </button>
             </div>
           </div>
         </div>
