626106a90596f588f24464cfb01b36be0e4ded41
Andy <olshansa@gmail.com>
2023-11-10 15:05:51 -0500

    Updated comments w/ local storage. Thanks Carla!

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Comment = ({ author, text, onDelete }) => {
+const Comment = ({ text, onDelete }) => {
     return (
         <div>
-            <h3>{author}</h3>
             <p>{text}</p>
             <button onClick={onDelete} className='rounded-full bg-sky-700 text-teal-50 text-xs px-3 py-1 text-md self-auto m-2 flex-none'>
                 Delete
@@ -13,26 +12,39 @@ const Comment = ({ author, text, onDelete }) => {
 }
 
 const CommentSection = () => {
-    const [comments, setComments] = useState([
-        { id: 1, text: 'Comment 1' },
-        { id: 2, text: 'Comment 2' },
-    ]);
+    // Initialize comments from localStorage or an empty array if no data is present
+    const [comments, setComments] = useState(() => {
+        const storedComments = localStorage.getItem('comments');
+        return storedComments ? JSON.parse(storedComments) : [
+            { id: 1, text: "This game is so fun! I can't wait to play together!!" },
+        ];
+    });
     const [newComment, setNewComment] = useState('');
 
+    // Save comments to localStorage whenever comments change
+    useEffect(() => {
+        localStorage.setItem('comments', JSON.stringify(comments));
+    }, [comments]);
+
     const addComment = () => {
-        setComments([...comments, { id: comments.length + 1, text: newComment }]);
+        const updatedComments = [
+            ...comments,
+            { id: comments.length + 1, text: newComment },
+        ];
+        setComments(updatedComments);
         setNewComment('');
     };
 
     const deleteComment = (id) => {
-        setComments(comments.filter((comment) => comment.id !== id));
+        const updatedComments = comments.filter((comment) => comment.id !== id);
+        setComments(updatedComments);
     };
 
     return (
         <div className='bg-white relative w-full p-4'>
-            <h1 className='text-2xl text-sky-950 text-center'>Comment Section</h1>
-            <div classname='flex justify-center w-full'>
-                <textarea value={newComment} onChange={(e) => setNewComment(e.target.value)} className='border-2 border-sky-950 p-1 flex-1 w-full left-0 self-center m-auto'/>
+            <h1 className='text-2xl text-sky-950 text-center p-1'>Comment Section</h1>
+            <div className='flex justify-center w-full'>
+                <textarea value={newComment} onChange={(e) => setNewComment(e.target.value)} className='border-2 border-sky-950 p-1 flex-1 w-full left-0 self-center m-auto' />
                 <button onClick={addComment} className='rounded-full bg-sky-900 text-teal-50 text-xs px-3 py-1 text-md self-auto m-2 flex-none'>
                     Add Comment
                 </button>
@@ -40,10 +52,14 @@ const CommentSection = () => {
 
             {comments.map((comment) => (
                 <div className='border-2 border-teal-100 my-3 p-1 w-full mx-auto shadow-md'>
-                    <Comment key={comment.id} text={comment.text} onDelete={() => deleteComment(comment.id)} />
+                    <Comment 
+                        key={comment.id}
+                        text={comment.text}
+                        onDelete={() => deleteComment(comment.id)}
+                    />
                 </div>
             ))}
-                      
+
         </div>
     );
 }
