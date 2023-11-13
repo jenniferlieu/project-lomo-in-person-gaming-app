1cc245aed403a4b3f65961b8e19e6118282373b6
Ziyike1 <tul51428@temple.edu>
2023-11-13 16:28:19 -0500

    change back the data type of migration

--- backend/database/migrations/2023_11_06_235042_create_profiles_table.php
+++ backend/database/migrations/2023_11_06_235042_create_profiles_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
             $table->text('about_me')->nullable();
-            $table->json('preferred_games')->nullable(); // use json type
-            $table->json('preference_tags')->nullable();  // use json type
+            $table->text('preferred_games')->nullable(); 
+            $table->text('preference_tags')->nullable(); 
             $table->timestampsTz();
         });
 
