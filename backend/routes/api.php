331d03b7cc127981c4c622c6ff23ea264512da98
Ziyike1 <tul51428@temple.edu>
2023-11-13 16:24:41 -0500

    add the route for ProfileController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeaconController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AttendeeController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +22,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('users', UserController::class)->except('store');
     Route::get('games', [GameController::class, 'getGame']);
 });
-Route::apiResource('attendees', AttendeeController::class);
-Route::post('attendees', [App\Http\Controllers\AttendeeController::class,'store']);
+
+Route::apiResource('profiles', ProfileController::class);
