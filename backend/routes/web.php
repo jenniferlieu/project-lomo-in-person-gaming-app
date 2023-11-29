5648e9dcf35052ebc2f15c77bd55b02b8acf4028
JonathanAltenburg <jaltenburg219@gmail.com>
2023-11-28 19:31:33 -0500

    Established login/register rate limit

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,5 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::middleware(['throttle:login'])->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::middleware(['throttle:register'])->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+});
