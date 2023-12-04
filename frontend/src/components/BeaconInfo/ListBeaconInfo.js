18598f5bee4d6378dda0eac7d1d1442ce1b222f9
del-cj <del.cj126@gmail.com>
2023-12-03 22:49:52 -0500

    minor style changes

--- frontend/src/components/BeaconInfo/ListBeaconInfo.js
+++ frontend/src/components/BeaconInfo/ListBeaconInfo.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import GetBeaconInfo from './GetBeaconInfo.js';
+import BeaconInfoWindow from '../Beacon/InfoWindow/BeaconInfoWindow.js';
 
 function formatTime(dateString) {
   const date = new Date(dateString);
@@ -12,24 +13,29 @@ function formatTime(dateString) {
   return strTime;
 }
 
+function openWindow(beacon) {
+  console.log('beacon clicked', {...beacon});
+  <BeaconInfoWindow {...beacon} />
+}
+
 function ListView() {
   const beaconList = GetBeaconInfo();
   console.log(beaconList);
 
   return (
     <div className='w-screen'>
-      <h1 className='text-center font-bold text-6xl text-sky-950'>Active Beacons</h1>
+      <h1 className='text-center text-shrink font-bold text-6xl text-sky-950'>Active Beacons</h1>
       {beaconList.map((beacon) =>
-        <div key={beacon.id} className='box-border w-11/12 m-auto my-3 p-2 border-2 rounded-md bg-white text-sky-950 flex'>
+        <div key={beacon.id}  className='box-border w-11/12 m-auto my-3 p-2 rounded-md bg-white text-sky-950 flex' onClick={() => openWindow(beacon)}>
           <div className='w-min-2/3 flex-grow text-left'>
             <p className='font-bold text-lg'>{beacon.game_title}</p>
             <p>{beacon.console}</p>
             <p className='font-bold text-md'>{beacon.place_name}</p>
             <p>{beacon.street_address}</p>
           </div>
-          <div className='w-1/6 text-center flex-col'>
-            <svg className='w-10 h-10 md:w-20 md:h-20 stroke-sky-200 fill-none mx-auto' viewBox='0 0 24 24'>
-              <path d="M12 7V12L14.5 10.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <div className='w-1/6 text-center flex-col mx-1'>
+            <svg className='w-10 h-10 md:w-20 md:h-20 stroke-sky-200 fill-none mx-auto stroke-2' viewBox='0 0 24 24'>
+              <path d="M12 7V12L14.5 10.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" />
             </svg>
             <div className='md:hidden'>
               <p>{formatTime(beacon.start_date_time)}</p>
