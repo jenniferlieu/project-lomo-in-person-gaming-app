96a7b0e282c3bfb9a84a5104c6477039394e5f87
Ziyike1 <tul51428@temple.edu>
2023-12-07 18:39:28 -0500

    fix the "login test" in AuthControllerTest

--- backend/tests/Feature/AuthControllerTest.php
+++ backend/tests/Feature/AuthControllerTest.php
@@ -12,50 +12,51 @@ class AuthControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    // public function testRegister()
-    // {
-    //     $userData = [
-    //         "email" => "test@example.com",
-    //         "password" => "password",
-    //         "password_confirmation" => "password",
-    //     ];
+    public function testRegister()
+    {
+        $userData = [
+            "email" => "test@example.com",
+            "password" => "password",
+            "password_confirmation" => "password",
+            "username" => "testuser"
+        ];
 
-    //     $response = $this->json('POST', '/api/register', $userData);
+        $response = $this->json('POST', '/api/register', $userData);
 
-    //     $response->assertStatus(201)
-    //         ->assertJsonStructure([
-    //             "data" => [
-    //                 "email",
-    //                 "username",
-    //                 "updated_at",
-    //                 "created_at",
-    //                 "id"
-    //             ]
-    //         ]);
-    // }
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                "data" => [
+                    "email",
+                    "username",
+                    "updated_at",
+                    "created_at",
+                    "id"
+                ]
+            ]);
+    }
 
-    // public function testLogin()
-    // {
-    //     $user = [
-    //         'email' => 'test@example.com',
-    //         'password' => Hash::make('password'),
-    //     ];
+    public function testLogin()
+    {
+        $user = User::create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'username' => 'testuser'
+        ]);
 
-    //     $loginData = ['email' => 'test@example.com', 'password' => 'password'];
+        $loginData = ['email' => 'test@example.com', 'password' => 'password'];
 
-    //     $response = $this->json('POST', '/api/login', $loginData);
+        $response = $this->json('POST', '/api/login', $loginData);
 
-    //     $response->assertStatus(200)
-    //         ->assertJsonStructure([
-    //             "token",
-    //             "user" => [
-    //                 "email",
-    //                 "username",
-    //                 "updated_at",
-    //                 "created_at",
-    //                 "id"
-    //             ]
-    //         ]);
-    // }
-
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                "token",
+                "user" => [
+                    "email",
+                    "username",
+                    "updated_at",
+                    "created_at",
+                    "id"
+                ]
+            ]);
+    }
 }
