7f9bca7127042288b1e383e7ab90f3f3e64862f3
Ziyike1 <tul51428@temple.edu>
2023-12-08 15:12:09 -0500

    finish the basic structure of GoogleApiController

--- backend/app/Http/Controllers/GoogleApiController.php
+++ backend/app/Http/Controllers/GoogleApiController.php
@@ -7,7 +7,18 @@ use Illuminate\Support\Facades\Http;
 class GoogleApiController extends Controller
 {
     public function googleApiRequest(Request $request)
-    {
-        // TODO
+    {   
+        //get the google api key from .env file
+        $realApiKey = env('GOOGLE_API_KEY');
+
+        // Define the actual Google API endpoint
+        $apiEndpoint = 'google-api-test.com';
+
+        $queryParams = [
+            'key' => $realApiKey,
+        ];
+
+        // Make the HTTP request to the Google API
+        $response = Http::get($apiEndpoint, $queryParams);
     }
 }
