72f00e669fd909acf5144b23bbdfcd069d899cab
Ziyike1 <tul51428@temple.edu>
2023-12-08 14:53:12 -0500

    create a new Controllers for google map api key

---
+++ backend/app/Http/Controllers/GoogleApiController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class GoogleApiController extends Controller
+{
+    public function googleApiRequest(Request $request)
+    {
+        // TODO
+    }
+}
