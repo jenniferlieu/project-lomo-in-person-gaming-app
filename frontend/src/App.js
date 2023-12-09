e0ed4a3da94e4109fa935f948b2b455f35a2f552
Andy <olshansa@gmail.com>
2023-12-09 15:44:10 -0500

    text change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -164,12 +164,13 @@ function App() {
             <div className="inline-block align-bottom bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle min-w-lg w-full max-w-xl">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <h2 className="text-3xl text-center leading-6 font-bold text-black">Warning</h2>
-                <div className="mt-2">
-                  <p className="text-xl font-semibold text-center pt-4 text-black">Don't be a dumbass</p>
+                <div className="mt-2 text-xl font-semibold text-center pt-4 text-black">
+                  <p>Beacons are visible to everyone</p>
+                  <p>Share your address at your own risk</p>
                 </div>
               </div>
               <div className="flex justify-center px-4 py-3 sm:px-6">
-                <button onClick={() => setShowModal(false)} type="button" className="w-full inline-flex justify-center rounded-full border border-transparent shadow-sm mb-4 px-20 py-4 bg-sky-600 text-lg font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+                <button onClick={() => setShowModal(false)} type="button" className="w-full inline-flex justify-center rounded-full border border-transparent shadow-sm mb-4 px-20 py-4 bg-sky-600 text-lg font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto">
                   Got it
                 </button>
               </div>
