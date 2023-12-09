0e28f1827ef8bca961221df1173287136ab807b7
Andy <olshansa@gmail.com>
2023-12-09 15:36:11 -0500

    Aligned elements better for the warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -157,19 +157,19 @@ function App() {
     <div className="App bg-gradient-to-b from-sky-500 to-teal-600 bg-scroll h-min-full h-screen">
       {showModal && (
         <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:p-0">
             <div className="fixed inset-0 transition-opacity" aria-hidden="true">
               <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="inline-block align-bottom bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle min-w-lg w-full max-w-xl">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <h2 className="text-lg leading-6 font-medium text-gray-900">Warning</h2>
+                <h2 className="text-3xl text-center leading-6 font-bold text-black">Warning</h2>
                 <div className="mt-2">
-                  <p className="text-sm text-gray-500">Be careful about sharing information.</p>
+                  <p className="text-xl font-semibold text-center pt-4 text-black">Don't be a dumbass</p>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button onClick={() => setShowModal(false)} type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+              <div className="flex justify-center px-4 py-3 sm:px-6">
+                <button onClick={() => setShowModal(false)} type="button" className="w-full inline-flex justify-center rounded-full border border-transparent shadow-sm mb-4 px-20 py-4 bg-sky-600 text-lg font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
                   Got it
                 </button>
               </div>
