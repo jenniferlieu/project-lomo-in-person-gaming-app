e8fae608b54389b3ae66b3ac6448911d01afcd10
Andy <olshansa@gmail.com>
2023-12-09 19:00:02 -0500

    workaround?

--- frontend/src/components/Beacon/Beacon.js
+++ frontend/src/components/Beacon/Beacon.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import { Circle, Marker } from '@react-google-maps/api';
-import BeaconInfoWindow from './InfoWindow/BeaconInfoWindow.js';
+import React from "react";
+import { Circle, Marker } from "@react-google-maps/api";
+import BeaconInfoWindow from "./InfoWindow/BeaconInfoWindow.js";
 
 const Beacon = ({ id, activeBeacon, onBeaconClick, beacon }) => {
   const circle = {
     center: {
-        lat: beacon.latitude,
-        lng: beacon.longitude
-      },
-    radius: 250, // Radius in meters 
+      lat: beacon.latitude,
+      lng: beacon.longitude,
+    },
+    radius: 250, // Radius in meters
     options: {
-      fillColor: '#FF0000', 
-      strokeColor: '#0000FF', 
-      strokeOpacity: 0.8, 
-      strokeWeight: 2, 
+      fillColor: "#FF0000",
+      strokeColor: "#0000FF",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
     },
   };
 
   const toggleDisplayBeacon = () => {
     onBeaconClick(id);
-  }
+  };
 
   return (
     <>
-    <Marker 
-        position={{ lat: beacon.latitude, lng: beacon.longitude }} 
+      <Marker
+        position={{ lat: beacon.latitude, lng: beacon.longitude }}
         onClick={toggleDisplayBeacon}
         icon={{
           url: beacon.game_image,
           scaledSize: new window.google.maps.Size(65, 65), // size of the icon
           anchor: new window.google.maps.Point(32.5, 32.5), // anchor point of the icon
-          labelOrigin: new window.google.maps.Point(32.5, 32.5), // origin of the label relative to the icon
-          label: {
-            text: ' ', // empty text
-            color: 'transparent', // transparent color
-            fontSize: '90px', // size of the border
-            fontWeight: '900', // thickness of the border
-            strokeWeight: 1, // border width
-            strokeColor: 'black', // border color
-          }
+        }}
+      />
+      <Marker
+        position={{ lat: beacon.latitude, lng: beacon.longitude }}
+        onClick={toggleDisplayBeacon}
+        icon={{
+          url: beacon.game_image,
+          scaledSize: new window.google.maps.Size(70, 70), // size of the icon
+          anchor: new window.google.maps.Point(35, 35), // anchor point of the icon
         }}
       />
       {activeBeacon === id && (
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <div className="w-screen max-w-[30rem]">
-            <BeaconInfoWindow {...beacon} onClose={toggleDisplayBeacon} id={id}/>
+            <BeaconInfoWindow
+              {...beacon}
+              onClose={toggleDisplayBeacon}
+              id={id}
+            />
           </div>
         </div>
       )}
@@ -53,9 +57,9 @@ const Beacon = ({ id, activeBeacon, onBeaconClick, beacon }) => {
 };
 
 Beacon.defaultProps = {
-    beaconInfo: BeaconInfoWindow.defaultProps,
-    circleLat: 40.1,
-    circleLng: -75.2
+  beaconInfo: BeaconInfoWindow.defaultProps,
+  circleLat: 40.1,
+  circleLng: -75.2,
 };
 
 export default Beacon;
